87296753c314490704fcbf8ccc6a5876f8d3e025
Alvin Ajay <alvinajay95@gmail.com>
2021-06-22 15:35:43 +0530

    removed redundant code

--- src/index.js
+++ src/index.js
@@ -2,13 +2,6 @@
 require('./styles/main.css')
 const axios = require('axios')
 
-let action = function(){
-    alert(action)
-} 
-let action2 = function(){
-    alert(action2)
-}
-
 function getMovie(search_value){
       axios.get('https://www.omdbapi.com/?s='+search_value+'&apikey=a5063d4f')
       .then((response)=>{
@@ -32,17 +25,13 @@ function getMovie(search_value){
     }
 
 //so its a scoping issue caused by babel wrapping the whole thing as a function
+//we'll have to define each function here and export it
 module.exports = {
     searchMovie: function () {      
       let search_value = document.getElementById("searchText").value
       getMovie(search_value)
       console.log(search_value);
-    },
-     
-    action,
-    action2,
-//we'll have to define each function here and export it
-
+    }, 
   };
 
 
